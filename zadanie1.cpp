383ea6ea802a4258e1e88db47736147b16f8ae4f
TomaszPodolski <90248201+TomaszPodolski@users.noreply.github.com>
2021-09-07 12:09:07 +0200

    Add files via upload

---
+++ zadanie1.cpp
@@ -0,0 +1,25 @@
+﻿// zadanie1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	int a;
+
+
+	for (a = 1; a <= 100; a++)
+	{
+		number = rand() % 100;
+		cout << number << endl;
+
+
+
+
+	}
+}
+
